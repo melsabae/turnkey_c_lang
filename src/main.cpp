e25f354250523775348a0e03d21ebdc60cf68d21
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2020-06-26 23:27:04 -0500

    testing things arent broked

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 #include "main.h"
+#include "test.h"
 
 int main(int argc, char** argv)
 {
   std::cout << __FUNCTION__ << std::endl;
+  function();
   return 0;
   (void) argc;
   (void) argv;
---
+++ src/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "test.h"
+
+
+void function(void)
+{
+  std::cout << __FUNCTION__ << std::endl;
+}
+
