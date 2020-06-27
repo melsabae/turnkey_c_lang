bd4771710ce48e1bda0ac58da8daa5d8401f7f4b
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2020-06-27 12:36:02 -0500

    auto-versioning based on git hashes

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "main.h"
+#include "version.h"
 
 
 int main(int argc, char** argv)
 {
   puts(__FUNCTION__);
+  puts(COMMIT_HASH);
   return 0;
   (void) argc;
   (void) argv;
--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 
 #include "main.h"
 #include "hi.h"
+#include "version.h"
 
 int main(int argc, char** argv)
 {
   std::cout << __FUNCTION__ << std::endl;
+  std::cout << COMMIT_HASH << std::endl;
   return 0;
   (void) argc;
   (void) argv;
